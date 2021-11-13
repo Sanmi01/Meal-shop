20283099bddd66764778c7d9da7ce422fc8961c4
Sanmi01 <supersanmi01@gmail.com>
2021-11-13 19:38:58 +0100

    cart modal implemented

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -3,14 +3,14 @@ import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = ({id, name, description, price}) => {
-    const amount = `$${price.toFixed(2)}`;
+    const pri = `$${price.toFixed(2)}`;
 
     return (
         <li className={classes.meal}>
             <div>
                 <h3>{name}</h3>
                 <div className={classes.description}>{description}</div>
-                <div className={classes.price}>{amount}</div>
+                <div className={classes.price}>{pri}</div>
             </div>
             <div>
                 <MealItemForm id={id} />
